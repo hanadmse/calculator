4317c890f142f98a2900d4d31bd67d2da50e9f89
hanadmse <hanadmse@gmail.com>
2024-06-12 19:57:47 -0500

    keypad and number display added

---
+++ script.js
@@ -0,0 +1,124 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+    let result = "";
+    if (operator === "add") {
+        result = add(num1, num2);
+    }
+    else if (operator === "subtract") {
+        result = subtract(num1, num2);
+    }
+    else if (operator === "multiply") {
+        result = multiply(num1, num2);
+    }
+    else {
+        result = divide(num1, num2);
+    }
+    return result;
+}
+let num1 = "";
+let num2 = "";
+let operator = "";
+let displayValue = "";
+
+
+const displayBtn = document.querySelector(".display");
+const clearBtn = document.querySelector("#clearBtn");
+const divBtn = document.querySelector("#divBtn");
+const multBtn = document.querySelector("#multBtn");
+const subBtn = document.querySelector("#subBtn");
+const addBtn = document.querySelector("#addBtn");
+const equalBtn = document.querySelector("#equalBtn");
+
+const sevenBtn = document.querySelector("#sevenBtn");
+const eightBtn = document.querySelector("#eightBtn");
+const nineBtn = document.querySelector("#nineBtn");
+const fourBtn = document.querySelector("#fourBtn");
+const fiveBtn = document.querySelector("#fiveBtn");
+const sixBtn = document.querySelector("#sixBtn");
+const threeBtn = document.querySelector("#threeBtn");
+const twoBtn = document.querySelector("#twoBtn");
+const oneBtn = document.querySelector("#oneBtn");
+
+
+clearBtn.addEventListener("click", () => {
+    displayBtn.textContent = "";
+});
+
+addBtn.addEventListener("click", () => {
+    num1 = parseInt(displayBtn.textContent, 10);
+    displayBtn.textContent = "";
+    operator = "add";
+});
+
+subBtn.addEventListener("click", () => {
+    num1 = parseInt(displayBtn.textContent, 10);
+    displayBtn.textContent = "";
+    operator = "subtract";
+});
+
+multBtn.addEventListener("click", () => {
+    num1 = parseInt(displayBtn.textContent, 10);
+    displayBtn.textContent = "";
+    operator = "multiply";
+});
+
+divBtn.addEventListener("click", () => {
+    num1 = parseInt(displayBtn.textContent, 10);
+    displayBtn.textContent = "";
+    operator = "divide";
+});
+
+equalBtn.addEventListener("click", () => {
+    if ((num1 !== "") && (operator !== "")) {
+        num2 = parseInt(displayBtn.textContent, 10);
+        displayBtn.textContent = "";
+    }
+    const calculation = operate(num1, num2, operator);
+    displayBtn.textContent = calculation;
+});
+
+sevenBtn.addEventListener("click", () => {
+    displayBtn.textContent += "7";
+});
+eightBtn.addEventListener("click", () => {
+    displayBtn.textContent += "8";
+});
+nineBtn.addEventListener("click", () => {
+    displayBtn.textContent += "9";
+});
+fourBtn.addEventListener("click", () => {
+    displayBtn.textContent += "4";
+});
+fiveBtn.addEventListener("click", () => {
+    displayBtn.textContent += "5";
+});
+sixBtn.addEventListener("click", () => {
+    displayBtn.textContent += "6";
+});
+threeBtn.addEventListener("click", () => {
+    displayBtn.textContent += "3";
+});
+twoBtn.addEventListener("click", () => {
+    displayBtn.textContent += "2";
+});
+oneBtn.addEventListener("click", () => {
+    displayBtn.textContent += "1";
+});
+zeroBtn.addEventListener("click", () => {
+    displayBtn.textContent += "0";
+});
